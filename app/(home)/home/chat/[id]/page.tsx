9661db871ac89f5f57e1d15d302a96489ed9e856
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-09-10 15:57:13 +0500

    Защита не своих чатов

--- app/(home)/home/chat/[id]/page.tsx
+++ app/(home)/home/chat/[id]/page.tsx
@@ -1,5 +1,7 @@
 import Chat from "@/components/chat/chat";
+import { authOptions } from "@/lib/auth";
 import { prisma } from "@/prisma/prisma-client";
+import { getServerSession } from "next-auth";
 
 const ChatPage = async ({
   params,
@@ -11,9 +13,23 @@ const ChatPage = async ({
   const id = Array.isArray(rawId) ? rawId[0] : rawId;
   const numericId = Number(id);
 
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return (
+      <div className="text-center mt-10">Пожалуйста, войдите в систему</div>
+    );
+  }
+
   const chat = await prisma.chat.findUnique({
     where: { id: numericId },
   });
+
+  if (session.user.id !== chat?.userId) {
+    return (
+      <div className="text-center mt-10">У вас нет доступа к этому чату</div>
+    );
+  }
+
   const messages = await prisma.message.findMany({
     where: { chatId: numericId },
     orderBy: { createdAt: "asc" },
